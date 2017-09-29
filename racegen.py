79b8011752266da234cdba276a58d46048d0ab8d
laisrael <laisrael@reed.edu>
2017-09-29 11:32:16 -0700

    Added Functionality to racegen
    Now will select a random core race or use the race input.

--- racegen.py
+++ racegen.py
@@ -1,4 +1,12 @@
 import random
 
+#Implement separate lists for the core races vs. special races
+races = ["Dwarf", "Elf", "Gnome", "Half-Elf", "Half-Orc", "Halfling", "Human"]
+
 def generate(inrace):
-	return "Just testing for now!"
+	if inrace == "rand":
+		race = random.choice(races)
+	else:
+		#Currently only supports Core Races
+		race = inrace
+	return race
