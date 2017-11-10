2cf329261059189e5a003358e9907d3da41a6409
laisrael <laisrael@reed.edu>
2017-11-10 09:52:29 -0800

    Added layout for future featgen update

--- featgen.py
+++ featgen.py
@@ -1,4 +1,44 @@
 import random
 
 def generate(myrace, myclass, mystats, infeats, inlevel):
+
+	# if infeats != "rand":
+	# 	return infeats
+
+	# elif myclass == "Barbarian":
+	# 	#Barbarian strength build
+
+	# elif myclass == "Bard":
+	# 	#Bard casting build
+
+	# elif myclass == "Cleric":
+	# 	#Cleric bruiser build
+
+	# elif myclass == "Druid":
+	# 	#Druid bruiser build
+
+	# elif myclass == "Fighter":
+	# 	#Fighter melee strength build
+
+	# elif myclass == "Monk":
+	# 	#Monk melee strength build
+
+	# elif myclass == "Paladin":
+	# 	#Paladin bruiser build
+
+	# elif myclass == "Ranger":
+	# 	#Ranger strength build
+
+	# elif myclass == "Rogue":
+	# 	#Rogue Dexterity assassin build
+
+	# elif myclass == "Sorcerer":
+	# 	#Sorcerer squishy control mage build
+
+	# elif myclass == "Wizard":
+	# 	#Wizard squishy control mage build
+
+	# else:
+	# 	#Account for input classes here, make an educated guess about feats based on stats
+
 	return ['Just', 'testing', 'for', 'now!']
