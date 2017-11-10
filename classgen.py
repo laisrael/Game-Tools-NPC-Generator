cf3dc0b51f9988a0b2afd08c02894a386f198bb1
laisrael <laisrael@reed.edu>
2017-11-10 09:44:42 -0800

    Input stats and classes now allowed

--- classgen.py
+++ classgen.py
@@ -3,17 +3,20 @@ import random
 def generate(inclass):
     #Using core classes only for proof of concept
     #Does not account for the flexibility between STR/DEX choices
-    classes = [
-               ('Barbarian', ['STR', 'CON', 'DEX', 'WIS', 'CHA', 'INT']),
-               ('Bard', ['CHA', 'CON', 'STR', 'DEX', 'WIS', 'INT']),
-               ('Cleric', ['WIS', 'CHA', 'CON', 'DEX', 'INT', 'STR']),
-               ('Druid', ['WIS', 'CON', 'STR', 'DEX', 'INT', 'CHA']),
-               ('Fighter', ['STR', 'CON', 'DEX', 'WIS', 'INT', 'CHA']),
-               ('Monk', ['STR', 'WIS', 'CON', 'DEX', 'INT', 'CHA']),
-               ('Paladin', ['CHA', 'WIS', 'STR', 'CON', 'DEX', 'INT']),
-               ('Ranger', ['STR', 'DEX', 'WIS', 'CHA', 'CON', 'INT']),
-               ('Rogue', ['DEX', 'CHA', 'CON', 'WIS', 'INT', 'STR']),
-               ('Sorcerer', ['CHA', 'DEX', 'CON', 'WIS', 'INT', 'STR']),
-               ('Wizard', ['INT', 'CON', 'DEX', 'WIS', 'CHA', 'STR'])
-               ]
-    return random.choice(classes)
+    if inclass != "rand":
+        return inclass
+    else:
+        classes = [
+                   ('Barbarian', ['STR', 'CON', 'DEX', 'WIS', 'CHA', 'INT']),
+                   ('Bard', ['CHA', 'CON', 'STR', 'DEX', 'WIS', 'INT']),
+                   ('Cleric', ['WIS', 'CHA', 'CON', 'DEX', 'INT', 'STR']),
+                   ('Druid', ['WIS', 'CON', 'STR', 'DEX', 'INT', 'CHA']),
+                   ('Fighter', ['STR', 'CON', 'DEX', 'WIS', 'INT', 'CHA']),
+                   ('Monk', ['STR', 'WIS', 'CON', 'DEX', 'INT', 'CHA']),
+                   ('Paladin', ['CHA', 'WIS', 'STR', 'CON', 'DEX', 'INT']),
+                   ('Ranger', ['STR', 'DEX', 'WIS', 'CHA', 'CON', 'INT']),
+                   ('Rogue', ['DEX', 'CHA', 'CON', 'WIS', 'INT', 'STR']),
+                   ('Sorcerer', ['CHA', 'DEX', 'CON', 'WIS', 'INT', 'STR']),
+                   ('Wizard', ['INT', 'CON', 'DEX', 'WIS', 'CHA', 'STR'])
+                   ]
+        return random.choice(classes)
--- statgen.py
+++ statgen.py
@@ -1,61 +1,65 @@
 import random
 
 def generate(instats, inlevel, racestats, statpref):
-	#Should this take into account classes that don't want to dump all their stat points in one stat?
 
-	#GENERATION
+	if instats != "rand":
+		return instats
 
-	#Randomly generate stat numbers
-
-	statNums = []
-	finalStats = []
-	i = 0
-	while i < 6:
-		diceRolled = 0
-		finalDice = 0
-		#Roll 4d6 and drop the lowest
-		die_1 = random.randint(1, 6)
-		die_2 = random.randint(1, 6)
-		die_3 = random.randint(1, 6)
-		die_4 = random.randint(1, 6)
-		dieList = [die_1, die_2, die_3, die_4]
-		dieList.remove(min(dieList))
-		for die in dieList:
-			finalDice += die
-		diceRolled += 1
-		statNums.append(finalDice)
-		i += 1
-
-	statNums.sort(reverse = True)
-
-	for num in statNums:
-		finalStats.append([num, statpref[0]])
-		statpref.remove(statpref[0])
-
-	#BONUSES
-
-	#Assign racial modifiers
-
-	if racestats == "ANY":
-		finalStats[0][0] += 2
 	else:
-		raceStatChanges = racestats.split(';')
-		for stat in raceStatChanges:
-			if stat[0] == "+":
-				changedStat = stat[1:]
-				for finalStat in finalStats:
-					if finalStat[1] == changedStat:
-						finalStat[0] += 2
-			else:
-				changedStat = stat[1:]
-				for finalStat in finalStats:
-					if finalStat[1] == changedStat:
-						finalStat[0] -= 2				
+		#Should this take into account classes that don't want to dump all their stat points in one stat?
+
+		#GENERATION
+
+		#Randomly generate stat numbers
+		statNums = []
+		finalStats = []
+		i = 0
+		while i < 6:
+			diceRolled = 0
+			finalDice = 0
+			#Roll 4d6 and drop the lowest
+			die_1 = random.randint(1, 6)
+			die_2 = random.randint(1, 6)
+			die_3 = random.randint(1, 6)
+			die_4 = random.randint(1, 6)
+			dieList = [die_1, die_2, die_3, die_4]
+			dieList.remove(min(dieList))
+			for die in dieList:
+				finalDice += die
+			diceRolled += 1
+			statNums.append(finalDice)
+			i += 1
+
+		statNums.sort(reverse = True)
+
+		for num in statNums:
+			finalStats.append([num, statpref[0]])
+			statpref.remove(statpref[0])
+
+		#BONUSES
+
+		#Assign racial modifiers
+
+		if racestats == "ANY":
+			finalStats[0][0] += 2
+		else:
+			raceStatChanges = racestats.split(';')
+			for stat in raceStatChanges:
+				if stat[0] == "+":
+					changedStat = stat[1:]
+					for finalStat in finalStats:
+						if finalStat[1] == changedStat:
+							finalStat[0] += 2
+				else:
+					changedStat = stat[1:]
+					for finalStat in finalStats:
+						if finalStat[1] == changedStat:
+							finalStat[0] -= 2				
 
 
-	#Assign level bonuses
+		#Assign level bonuses
 
-	abilityBonus = inlevel // 4
-	finalStats[0][0] += abilityBonus
+		abilityBonus = inlevel // 4
+		finalStats[0][0] += abilityBonus
 
-	return finalStats
+		return finalStats
