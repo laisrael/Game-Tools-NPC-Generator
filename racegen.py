0921a4bd369f228059bd1d693bb8a9b6f2d791c1
Laura Israel <myanya001@gmail.com>
2018-07-11 16:13:03 -0700

    Removed functional logic from racegen

--- racegen.py
+++ racegen.py
@@ -3,14 +3,6 @@ import random
 #Implement separate lists for the core races vs. special races
 races = [("Dwarf", "+CON;+WIS;-CHA"), ("Elf", "+DEX;+INT;-CON"), ("Gnome", "+CON;+CHA;-STR"), ("Half-Elf", "ANY"), ("Half-Orc", "ANY"), ("Halfling", "+DEX;+CHA;-STR"), ("Human", "ANY")]
 
-def generate(inrace):
-	if inrace == "rand":
-		race = random.choice(races)
-	else:
-		#Currently only supports Core Races
-		race = inrace
-	return race
-
 class Race():
     def __init__(self, inRace):
         if inRace == "rand":
